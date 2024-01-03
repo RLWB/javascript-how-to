1288e0c3ee21bf6eaf34055c736b0dcbfe506055
RLWB <rlwb1017669154@gmail.com>
2024-01-03 22:44:55 +0800

    feat: add throttle function code

--- 1.debounce-and-throttle/throttle.js
+++ 1.debounce-and-throttle/throttle.js
@@ -1,17 +1,16 @@
 /**
- * @function debounce
+ * @function throttle
  * @param {function} fn
  * @param {number} delay
  */
-const debounce = (fn, delay) => {
+const throttle = (fn, delay) => {
   let timer;
   return (...args) => {
     let ctx = this;
-    if (timer) {
-      clearTimeout(timer);
-    }
+    if (timer) return;
     timer = setTimeout(() => {
-      fn.apply(ctx, args);
+      fn.apply(ctx, agrs);
+      timer = null;
     }, delay);
   };
 };
