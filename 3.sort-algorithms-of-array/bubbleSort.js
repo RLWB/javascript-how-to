ae36bc6eefa2eb63dce7eda97fe6dcf309686f36
RLWB <rlwb1017669154@gmail.com>
2024-01-08 22:43:28 +0800

    feat: add how to implement Sorting Algorithmsand give bubble sort method.

---
+++ 3.sort-algorithms-of-array/bubbleSort.js
@@ -0,0 +1,17 @@
+/**
+ * @function bubbleSort
+ * @param {array} arr
+ */
+const bubbleSort = (arr) => {
+  let len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - j; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
