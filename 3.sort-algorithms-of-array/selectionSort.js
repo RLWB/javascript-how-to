c35e9b1e25a2ac0f0e605b48859035c41fad7ad1
RLWB <rlwb1017669154@gmail.com>
2024-01-08 22:50:15 +0800

    feat: add selection sort algorithm code

---
+++ 3.sort-algorithms-of-array/selectionSort.js
@@ -0,0 +1,21 @@
+/**
+ * @description JavaScript selection sort algorithm
+ * @param {array} arr
+ * @return {array}
+ */
+const selectionSort = (arr) => {
+  let len = arr.length;
+  let minIndex, temp;
+  for (let i = 0; i < len - 1; i++) {
+    minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+  return arr;
+};
